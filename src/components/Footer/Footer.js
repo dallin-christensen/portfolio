6a936d380792821dccc9327a46b4161b74f40cd2
Dallin Christensen <dallin@pyxiscoin.com>
2018-07-24 08:42:32 -0700

    footer icons built out

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import styled from 'react-emotion'
 import { darkGrey, white } from '../../utils/colors'
+import { FaGithubAlt, FaLinkedin } from 'react-icons/lib/fa'
 
 const Container = styled('div')`
   height: 300px;
@@ -13,11 +14,38 @@ const Container = styled('div')`
   align-items: center;
 `
 
+const Icons = styled('div')`
+  width: 250px;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-evenly;
+  align-items: center;
+`
+const IconLink = styled('a')`
+  text-decoration: none;
+  color: ${white};
+  font-size: 70px;
+  padding: 10px;
+  cursor: pointer;
+  transition: all .1s ease-in-out;
+  &:hover {
+    padding: 0;
+    font-size: 90px;
+  }
+`
+
 class Footer extends Component {
   render() {
     return (
       <Container>
-        footer
+        <Icons>
+          <IconLink href='https://github.com/dallin-christensen'>
+            <FaGithubAlt />
+          </IconLink>
+          <IconLink href='https://www.linkedin.com/in/dallinpchristensen/'>
+            <FaLinkedin />
+          </IconLink>
+        </Icons>
       </Container>
     )
   }
