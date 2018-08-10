9f6e56849ebcdd27a769f403b3843a410160c5cd
Dallin Christensen <dallin@pyxiscoin.com>
2018-08-10 08:58:10 -0700

    adding fixed position title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import styled from 'react-emotion'
 import { Banner, Projects, Skills, Footer } from './components'
+import { blue } from './utils/colors'
 
 const Container = styled('div')`
   padding: 0;
@@ -8,10 +9,30 @@ const Container = styled('div')`
   font-family: 'Roboto', sans-serif;
 `
 
+const MovingHeader = styled('div')`
+  height: 150px;
+  width: 150px;
+  border-radius: 50%;
+  background-color: ${blue};
+  opacity: 0.99;
+  color: white;
+  position: fixed;
+  top: 100px;
+  right: 50px;
+  border: 5px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
+
 class App extends Component {
   render() {
     return (
       <Container>
+        <MovingHeader>
+          <div>PROJECTS &</div><div>OPEN SOURCE</div>
+        </MovingHeader>
         <Banner />
         <Projects />
         <Skills />
