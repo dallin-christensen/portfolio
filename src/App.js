5dbd48360570ac239809a1801869a20d4abba84d
Dallin Christensen <dallin@pyxiscoin.com>
2018-08-13 08:07:37 -0700

    took out MovingHeader

--- src/App.js
+++ src/App.js
@@ -9,30 +9,10 @@ const Container = styled('div')`
   font-family: 'Roboto', sans-serif;
 `
 
-const MovingHeader = styled('div')`
-  height: 150px;
-  width: 150px;
-  border-radius: 50%;
-  background-color: ${blue};
-  opacity: 0.99;
-  color: white;
-  position: fixed;
-  top: 100px;
-  right: 50px;
-  border: 5px solid white;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-`
-
 class App extends Component {
   render() {
     return (
       <Container>
-        <MovingHeader>
-          <div>PROJECTS &</div><div>OPEN SOURCE</div>
-        </MovingHeader>
         <Banner />
         <Projects />
         <Skills />
