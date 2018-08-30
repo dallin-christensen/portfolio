6a298163bbb40b7df3d38e55bda5c6df99ba0c15
Dallin Christensen <dallin@pyxiscoin.com>
2018-08-30 08:28:24 -0700

    textures added

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,6 +12,7 @@ const Container = styled('div')`
   flex-flow: row nowrap;
   justify-content: center;
   align-items: center;
+  background-image: url("data:image/svg+xml,%3Csvg width='6' height='6' viewBox='0 0 6 6' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23323f4e' fill-opacity='0.4' fill-rule='evenodd'%3E%3Cpath d='M5 0h1L0 6V5zM6 5v1H5z'/%3E%3C/g%3E%3C/svg%3E");
 `
 
 const Icons = styled('div')`
